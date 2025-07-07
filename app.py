eeaf76f4d054e0971ac37eae06001645c7b913ea
Saleel Nayak <anuragh0413@gmail.com>
2025-07-07 15:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -930,7 +930,9 @@ def test_db():
     except Exception as e:
         return f"❌ DB Error: {str(e)}"
 
-pass
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
     
