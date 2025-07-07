f735cf6250c300b8d418bdc3eb6d1ce88e0f6ef8
Saleel Nayak <anuragh0413@gmail.com>
2025-07-07 14:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -934,6 +934,5 @@ def expense():
 import os
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
     
